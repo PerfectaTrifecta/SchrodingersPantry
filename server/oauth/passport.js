79acf6f2976574ff63dee64d80f57d4c8de5b535
Anastasia <anarenee9696@gmail.com>
2022-02-14 17:22:58 -0600

    configured passport

---
+++ server/oauth/passport.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+},
+(accessToken, refreshToken, profile, cb) => {
+  cb(err, profile);
+}));
+
+module.exports = {
+  GoogleStrategy,
+};
