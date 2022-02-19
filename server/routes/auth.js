c9630ca7ba7e489a35ddf4e43bad3d788e7181e3
Tr3 Isaac <oisaac47@gmail.com>
2022-02-19 12:26:50 -0600

    added spotify auth routes

---
+++ server/routes/auth.js
@@ -0,0 +1,19 @@
+const axios = require('axios').default;
+const { Router } = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
+
+let spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+let spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+const authRouter = Router();
+
+authRouter.get('/login', (req, res) => {
+
+});
+
+authRouter.get('/login', (req, res) => {
+
+});
+
+module.exports = { authRouter };
