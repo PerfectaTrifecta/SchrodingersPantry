aa9b6ae0f2e46377054d52c32fd3e686f97ea096
keithfrederick1 <kfrederick1004@gmail.com>
2022-02-23 10:24:58 -0800

    updating/ wop in userRouter, not done

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,7 @@
 const axios = require('axios').default;
 const { Router } = require('express');
+const { User } = require('../db');
+
 
 
 const UserRouter = Router();
@@ -7,17 +9,29 @@ const UserRouter = Router();
 
 //on User post of image
 
+const handleUpload = async ({ formData }) => {
+  await axios.post('https://api.cloudinary.com/v1_1/schrodinger-s-pantry/image/upload', formData)
+        .then((response) => {
+          setShownImage(response.data.url);
+        });
+};
+
+
+
+
+
+
 //const CLOUDINARY_API_KEY = process.env;
-UserRouter.post('/profile', (req, res) => {
+UserRouter.patch('/image/upload', (req, res) => {
   //take userID from req.params and file from req.body
-  const { userId } = req.params, file = req.body;
+  const { userId } = req.params, formData = req.body;
   //make axios post request with file, get back secret to store in database
-  axios.post('cloudinaryAPI')
+  handleUpload()
     .then((data) => {
-      //save cloudinary secret to user in database
+      //save cloudinary url to user in database
       //send back file and 201 
       res.status = 201;
-      res.send(file)
+      res.send(formData)
     })
     .catch((err) => {
       console.err(`could not upload: ${err}`);
@@ -27,13 +41,17 @@ UserRouter.post('/profile', (req, res) => {
 
 //on User's return to profile page
 
-UserRouter.get('/profile', (req, res) => {
-  const { userId } = req.params;
+UserRouter.get('/', (req, res) => {
+  const { googleId } = req.params;
   //search database for user with that id (SEQUELIZE POSTGRES syntax)
   //get the cloudinary secret ID for their image
 
-  axios.get('cloudnaryAPI with image id as param')
-    .then((data) => {
+  //access that User's profile image url, if they dont have one return 404
+  User.findOne({where : {
+    googleId : googleId
+  }})
+    .then((user) => {
+      //send the whole user back to front end for context
       //take the file from data
       //send the file with a 200 status
       res.status = 200;
