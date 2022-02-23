3975761077244335340f6fd6be9b44eb731c0b93
Tr3 Isaac <oisaac47@gmail.com>
2022-02-23 08:20:46 -0600

    delete comments from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,4 @@
-// import React, { useState, useEffect} from 'react';
-// import Search from './Search';
-// import Login from './spotify/Login';
-// import WebPlayback from './spotify/WebPlayback';
-// import axios from 'axios'
 
-// interface TokenValue {
-//   token: String
-// }
-
-// const App: React.FC = (): JSX.Element => {
-//   const [token, setToken] = useState(undefined);
-  
-
-//   useEffect(() => {
-
-//     async function getToken() {
-//       const response = axios.get('/auth/token').then(res => {
-//         setToken(res.data.accessToken)
-//       });
-//       // setToken(json.access_token);
-
-//     }
-
-//     getToken();
-//   }, []);
-  
-//   return (
-//     <div>
-//       {' '}
-//       What up World
-//       {/* <Search /> */}
-//          { token === undefined ? <Login/> : <WebPlayback token={token}/> }
 import React, { useState, useContext } from 'react';
 import CreateRecipeForm from './components/Profile/CreateRecipeForm';
 import HomePage from './components/Home Page/HomePage';
