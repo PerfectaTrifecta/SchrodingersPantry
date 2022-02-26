a31fb9370f8eb92ef1448dc346f6ea9ccf8009b2
Riley Katz <riley.davis.katz@gmail.com>
2022-02-26 13:02:53 -0600

    blow up timer and start over

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -1,63 +1,44 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React, {useState}  from 'react';
+import moment from 'moment';
+ 
+const Timer = () => {
 
-const Timer: React.FC = () => {
+    const [minutes, setMin] = useState("");
+    const [newTime2, setNewTime] = useState(1000);
 
-  
-  const [ minutes, setMinutes ] = useState(0);
-  const [seconds, setSeconds ] =  useState(0);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let test = moment.duration({
+          minutes: 0,
+          seconds: 5,
+        });
+         let seconds = test.asMilliseconds();
+         setInterval(() => {
+             seconds = seconds - 1000;
+             if(newTime2 === 0){
+               clearInterval();
+             } else {
+               setNewTime(seconds);
+               console.log('CountDown', seconds) //if this === oldTime, then stop
+             }
+         }, 1000);
 
-  const startTimer = () => {
-      const int = setInterval(() => {
-              if (seconds > 0) {
-                  setSeconds(seconds - 1);
-              }
-              if (seconds === 0) {
-                  if (minutes === 0) {
-                      clearInterval(int)
-                  } else {
-                      setMinutes(minutes - 1);
-                      setSeconds(59);
-                  }
-              }
-          }, 1000)
-          return ()=> {
-              clearInterval(int);
-          };
-  }
+    }
 
-
-  const onMinChange = (e: any) => {
-      const re = /^[0-9\b]+$/;
-      if (e.target.value === '' || re.test(e.target.value)) {
-         setMinutes(e.target.value);
-          }
-  }
-  
- useEffect(() => {
-     startTimer();
- })
-
-  const onSecChange = (e: any) => {
-    const re = /^[0-9\b]+$/;
-    if (e.target.value === '' || re.test(e.target.value)) {
-       setSeconds(e.target.value);
-        }
-}
-
-          
     return (
-        <div>
-          <h1>Set A Timer: </h1>
-          <input type='text' onChange={onMinChange} />
-          <input type='text' onChange={onSecChange} />
-          <button onClick={startTimer}>Start</button>
-          
-        { minutes === 0 && seconds === 0
-            ? <h1>Food is Ready!</h1>
-            : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
-        }
-        </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+        <label>
+          minute:
+          <input
+            type="text"
+            value={minutes}
+            onChange={e => setMin(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
     )
 }
 
