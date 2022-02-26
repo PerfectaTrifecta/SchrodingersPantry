d249e68cf88d9c070f05eb5d8dc313f3f0c60fc1
Riley Katz <riley.davis.katz@gmail.com>
2022-02-26 13:52:28 -0600

    add functional timer

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -3,24 +3,31 @@ import moment from 'moment';
  
 const Timer = () => {
 
-    const [minutes, setMin] = useState("");
-    const [newTime2, setNewTime] = useState(1000);
+    const [minutes, setMins] = useState(0);
+    const [seconds, setSecs] = useState(0);
+    const [newTime, setNewTime] = useState(0);
+    const [secondsRemaining, setSecondsRemaining] = useState(0);
+    const [minutesRemaining, setMinutesRemaining] = useState(0);
+
+
+    const test = moment.duration({
+      minutes,
+      seconds,
+    });
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        let test = moment.duration({
-          minutes: 0,
-          seconds: 5,
-        });
-         let seconds = test.asMilliseconds();
-         setInterval(() => {
-             seconds = seconds - 1000;
-             if(newTime2 === 0){
-               clearInterval();
-             } else {
-               setNewTime(seconds);
-               console.log('CountDown', seconds) //if this === oldTime, then stop
-             }
+        // e.preventDefault();
+         let milliseconds = test.asMilliseconds();
+         const timeKeeper = setInterval(() => {
+            milliseconds = milliseconds - 1000;
+            setNewTime(milliseconds);
+            setMinutesRemaining(Math.floor(milliseconds / 60000));
+            setSecondsRemaining(Math.floor(milliseconds % 60000) / 1000);
+            if(milliseconds === 0) {
+              clearInterval(timeKeeper);
+              setMinutesRemaining(0);
+              setSecondsRemaining(0);
+            }
          }, 1000);
 
     }
@@ -29,15 +36,24 @@ const Timer = () => {
       <div>
         <form onSubmit={handleSubmit}>
         <label>
-          minute:
+          minutes:
           <input
             type="text"
             value={minutes}
-            onChange={e => setMin(e.target.value)}
+            onChange={e => setMins(Number(e.target.value))}
+          />
+        </label>
+        <label>
+          seconds:
+          <input
+            type="text"
+            value={seconds}
+            onChange={e => setSecs(Number(e.target.value))}
           />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <h1>Time Remaining: {minutesRemaining}:{secondsRemaining}</h1>
       </div>
     )
 }
