1601382a275b821269ac4782f832546fa6f702ad
Tr3 Isaac <oisaac47@gmail.com>
2022-03-09 20:37:53 -0600

    render/populate info window on click

--- client/src/components/marketLocator/map.tsx
+++ client/src/components/marketLocator/map.tsx
@@ -28,8 +28,8 @@ const Map: React.FC<MapProps> = ({
   const ref = React.useRef<HTMLDivElement>(null);
   const [map, setMap] = React.useState<google.maps.Map>();
   const [zip, setZip] = React.useState<string>('');
-
-  
+  const [marker, setMarker] = React.useState<google.maps.Marker>()
+  const [infoWindow, setInfoWindow] = React.useState<google.maps.InfoWindow>()
 
   React.useEffect(() => {
     if (ref.current && !map) {
@@ -47,7 +47,17 @@ const Map: React.FC<MapProps> = ({
       );      
     }
   }, [map, onIdle]);
-
+  React.useEffect(() => {
+    if(marker){
+      marker.addListener("click", () => {
+        infoWindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
+    }
+  }, [marker, infoWindow])
   
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -57,27 +67,31 @@ const Map: React.FC<MapProps> = ({
     axios
       .get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${zip}`)
       .then(({ data }) => {
-        console.log(data.results);
         const top3 = data.results.slice(0, 3)
 
         for (let i = 0; i < top3.length; i++) {
 
           return axios.get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=${top3[i].id}`)
             .then(response => {
-              addresses.push(response.data.marketdetails.Address);
+              addresses.push(response.data.marketdetails);
           })
         })
       .then(res => {
-        return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addresses[0]}&key=${process.env.GOOGLE_MAPS_API_KEY}`)
+        return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${addresses[0].Address}&key=${process.env.GOOGLE_MAPS_API_KEY}`)
 
 
       }).then(res => {
-        const latLng = new google.maps.LatLng(res.data.results[0].geometry.location);
+        const contentString = `${addresses[0].Address} ${addresses[0].Schedule}`;
+    
 
-        new google.maps.Marker({
-          position: latLng,
+        setInfoWindow(new google.maps.InfoWindow({
+          content: contentString,
+        }));
+      setMarker( new google.maps.Marker({
+          position: res.data.results[0].geometry.location,
           map: map,
-        });
+        }));
+      
         console.log(res.data.results[0]);
         setCenter(res.data.results[0].geometry.location);
         setZoom(15);
@@ -86,10 +100,16 @@ const Map: React.FC<MapProps> = ({
         console.error(err);
       });
 
+   
+      
+
 
 
 
   };
+  
+
+  
 
   const handleInput = (e: any) => {
     e.preventDefault();
