0769e88f80a62ce893425a993cd4849fc3b8e902
Zack Izdepski <zizdep1@gmail.com>
2022-04-05 12:30:51 -0500

    Added new logo

--- client/src/components/Home Page/PulloutMenu.tsx
+++ client/src/components/Home Page/PulloutMenu.tsx
@@ -34,7 +34,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import { Link } from 'react-router-dom';
-import logo from '../../img/logo.png';
+import schrodingers_logo from '../../img/schrodingers_logo_black.png';
 
 interface TokenValue {
   token: string;
@@ -113,7 +113,11 @@ const PulloutMenu: React.FC<Props> = ({ changeTheme }) => {
       }}
     >
       <Link to={'/'}>
-        <img src={logo} width='200' style={{ paddingTop: '20px' }} />
+        <img
+          src={schrodingers_logo}
+          width='180'
+          style={{ paddingTop: '20px' }}
+        />
       </Link>
       {loggedIn ? (
         <List
