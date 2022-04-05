9195fd5884a9e68c8d591072ea3ce6f134c02931
student <student@MacBook-Pro-7.lan>
2022-04-05 09:48:06 -0500

    img upload styled to profile

--- client/src/components/Profile/ProfileImage.tsx
+++ client/src/components/Profile/ProfileImage.tsx
@@ -2,36 +2,34 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import axios from 'axios';
 
-
 const ProfileImage = () => {
+  const [image, setImage] = React.useState<File | File[]>([]);
+  const [displayImg, setDisplayImg] = React.useState<string>(
+    'http://res.cloudinary.com/schrodinger-s-pantry/image/upload/v1649119002/on2sre4jrjtrgatzovbk.png'
+  );
 
-  const [image, setImage] = React.useState< File | File[]>([]);
-  const [displayImg, setDisplayImg] = React.useState<string>('http://res.cloudinary.com/schrodinger-s-pantry/image/upload/v1649119002/on2sre4jrjtrgatzovbk.png');
-
-
-  const handleUpload = async ( file: File | File[]) : Promise<void> => {
-    if(file && !Array.isArray(file)) {
-    const formData = new FormData();
+  const handleUpload = async (file: File | File[]): Promise<void> => {
+    if (file && !Array.isArray(file)) {
+      const formData = new FormData();
       formData.append('file', file);
-      formData.append('upload_preset', 'ivfzsgyx') 
+      formData.append('upload_preset', 'ivfzsgyx');
 
-      axios.post('https://api.cloudinary.com/v1_1/schrodinger-s-pantry/image/upload',
-       formData).then(({ data }) => {
-        setDisplayImg(data.url)
+      axios
+        .post(
+          'https://api.cloudinary.com/v1_1/schrodinger-s-pantry/image/upload',
+          formData
+        )
+        .then(({ data }) => {
+          setDisplayImg(data.url);
 
-        console.log(12345667, data);
-       }).catch(() => {
-
-       });
-
-
-
-
+          console.log(12345667, data);
+        })
+        .catch(() => {});
     }
-  }
+  };
   return (
     <div>
-      <Box 
+      <Box
         sx={{
           width: 300,
           height: 300,
@@ -42,23 +40,26 @@ const ProfileImage = () => {
           },
         }}
       >
-        <img src={displayImg} alt="profile"  width='300'
-          height='300' />
+        <img src={displayImg} alt='profile' width='300' height='300' />
       </Box>
-      <div> 
-        <input type="file" 
-        accept="image/*"
-         multiple={false}
-         onChange={(event) => {
+      <div>
+        <input
+          type='file'
+          accept='image/*'
+          multiple={false}
+          onChange={(event) => {
             setImage(event.target.files[0]);
-         }} />
+          }}
+        />
       </div>
-         <button onClick={() => {
-           handleUpload(image);
-         }}>Upload</button>
-
+      <button
+        onClick={() => {
+          handleUpload(image);
+        }}
+      >
+        Upload
+      </button>
     </div>
-    
   );
 };
 export default ProfileImage;
